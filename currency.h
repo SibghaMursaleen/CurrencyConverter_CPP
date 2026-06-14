e1fa18ef65ff5b748b24672f0c5d0132e44742ad
Sibgha Mursaleen <sibgham55@gmail.com>
2026-06-14 15:30:51 +0500

    Add files via upload

---
+++ currency.h
@@ -0,0 +1,122 @@
+// currency library
+// it has user defined functions
+// these function convert the pkr to respective currency 
+
+long double dollar(long double pkr)
+{
+	long double result = 0; 
+	result = pkr / 286.667 ; 
+	return result; 
+}
+
+long double euro(long double pkr) 
+{
+	long double result = 0;
+	result = pkr / 306.839; 
+	return result; 
+}
+long double riyals(long double pkr) 
+{
+	long double result = 0;  
+	result = pkr / 76.4377; 
+	return result; 
+
+}
+long double DKK(long double pkr) 
+{
+	long double result = 0;  
+	result = pkr / 41.1840 ; 
+	return result; 
+
+}
+long double britishpound(long double pkr)
+{
+	long double result = 0;
+	result = pkr / 356.801 ;
+	return result ;
+}
+long double TRY(long double pkr)
+{
+	long double result = 0;
+	result = pkr / 12.3496  ;
+	return result ;
+}
+long double CanDollar(long double pkr)
+{
+	long double result = 0 ;
+	result = pkr / 214.481 ;
+	return result ;
+}
+long double norway(long double pkr)
+{
+	long double result = 0;
+	result = pkr / 25.9696  ;
+	return result;
+}
+long double INR(long double pkr)
+{
+	long double result = 0 ;
+	result = pkr / 3.47132 ; 
+	return result ;
+}
+long double Ausdollar(long double pkr)
+{
+	
+	long double result = 0 ;
+	result = pkr / 190.940 ; 
+	return result ;
+}
+long double japan(long double pkr)
+{
+	
+	long double result = 0 ;
+	result = pkr / 2.04770 ; 
+	return result ;
+}
+long double georgia(long double pkr)
+{
+	
+	long double result = 0 ;
+	result = pkr / 109.820 ; 
+	return result ;
+}
+long double france(long double pkr)
+{
+	
+	long double result = 0 ;
+	result = pkr / 46.7345  ; 
+	return result ;
+}
+long double UAE(long double pkr)
+{
+	
+	long double result = 0 ;
+	result = pkr /  78.0570 ; 
+	return result ;
+}
+long double SEK(long double pkr)
+{
+	
+	long double result = 0 ;
+	result = pkr / 26.2773 ; 
+	return result ;
+} 
+long double CNY(long double pkr)
+{
+	
+	long double result = 0 ;
+	result = pkr / 40.2175 ; 
+	return result ;
+}
+long double qatar(long double pkr)
+{
+	long double result = 0 ;
+	result = pkr /  78.7526 ;
+	return result ;
+} 
+long double iraq(long double pkr)
+{
+	long double result = 0 ;
+	result = pkr /  0.218998 ;
+	return result ;
+} 
